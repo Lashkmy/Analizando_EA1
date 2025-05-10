693f08bf3bd2e98d23e9a424042b84d69a4ac0f9
Lashkmy <indira.hamdam@est.iudigital.edu.co>
2025-05-10 14:05:59 -0500

    Agrega archivo setup.py para configuración inicial del proyecto

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Analizando_EA1",
+    version="0.0.1",
+    author="Indira Hamdam",
+    author_email="indira.hamdam@est.iudigital.edu.co",
+    description="Proyecto educativo de obtención de datos basado en Web Scraping con BeautifulSoup para extraer, procesar y visualizar información de las 250 mejores películas según IMDb.",
+    py_modules=[""],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "beautifulsoap4",
+    ]
+)
